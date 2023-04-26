0add2cccf240ac7b491c5aeef2254ef0573e70e6
IwanDywan <aleksander.iwaniak@gmail.com>
2023-04-27 00:23:10 +0200

    Dodanie informacji o użytkowniku do bazy danych podczas rejestracji oraz usuwanie jego danych podczas usuwania konta

--- app/src/main/java/com/example/zuberek/MainActivity.java
+++ app/src/main/java/com/example/zuberek/MainActivity.java
@@ -4,26 +4,59 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button logout;
+    private Button delete;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    private CollectionReference usersRef = db.collection("Users");
+
+    private FirebaseAuth auth;
+
+    private static final String KEY_USERNAME = "username";
+    private static final String KEY_EMAIL = "email";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         logout = findViewById(R.id.buttonLogout);
+        delete = findViewById(R.id.buttonDeleteAcc);
+        auth = FirebaseAuth.getInstance();
 
         logout.setOnClickListener(view -> {
             FirebaseAuth.getInstance().signOut();
             startActivity(new Intent(MainActivity.this, StartActivity.class));
             Toast.makeText(MainActivity.this, "Wylogowano!", Toast.LENGTH_SHORT).show();
+        });
+
+        delete.setOnClickListener(view -> {
+            String usersUid = auth.getCurrentUser().getUid();
+            auth.getCurrentUser().delete().addOnCompleteListener(MainActivity.this, task -> {
+                if(task.isSuccessful())
+                {
+                    usersRef.document(usersUid).delete();
+                    FirebaseAuth.getInstance().signOut();
+                    startActivity(new Intent(MainActivity.this, StartActivity.class));
+                    Toast.makeText(MainActivity.this, "Usunięto konto!", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(MainActivity.this, "Usunięcie konta nieudane!", Toast.LENGTH_SHORT).show();
+                }
+            });
         });
     }
 }
--- app/src/main/java/com/example/zuberek/RegisterActivity.java
+++ app/src/main/java/com/example/zuberek/RegisterActivity.java
@@ -15,13 +15,22 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
 
     private EditText email;
     private EditText password;
+    private EditText username;
     private Button register;
     private FirebaseAuth auth;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private static final String KEY_USERNAME = "username";
+    private static final String KEY_EMAIL = "email";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,25 +39,32 @@ public class RegisterActivity extends AppCompatActivity {
 
     email = findViewById(R.id.editTextEmail);
     password = findViewById(R.id.editTextPassword);
+    username = findViewById(R.id.editTextUsername);
     register = findViewById(R.id.buttonRegister);
     auth = FirebaseAuth.getInstance();
 
     register.setOnClickListener(view -> {
         String txtEmail = email.getText().toString();
         String txtPassword = password.getText().toString();
+        String txtUsername = username.getText().toString();
 
-        if(TextUtils.isEmpty(txtEmail) || TextUtils.isEmpty(txtPassword)){
+        if(TextUtils.isEmpty(txtEmail) || TextUtils.isEmpty(txtPassword) || TextUtils.isEmpty(txtUsername)){
             Toast.makeText(RegisterActivity.this, "Nie podano danych", Toast.LENGTH_SHORT).show();
         }else if (txtPassword.length()<6){
             Toast.makeText(RegisterActivity.this, "Hasło musi mieć przynajmniej 6 znaki", Toast.LENGTH_SHORT).show();
         }else{
-            registerUser(txtEmail,txtPassword);
+            registerUser(txtEmail,txtPassword, txtUsername);
         }
     });
     }
-    private void registerUser(String email, String password) {
-        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(RegisterActivity.this, task -> {
+    private void registerUser(String email, String password, String username) {
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(RegisterActivity.this, task -> {
             if(task.isSuccessful()){
+                Map<String, Object> userData = new HashMap<>();
+                userData.put(KEY_USERNAME, username);
+                userData.put(KEY_EMAIL, email);
+                db.collection("Users").document(auth.getCurrentUser().getUid()).set(userData);
                 Toast.makeText(RegisterActivity.this, "Zarejestrowano!", Toast.LENGTH_SHORT).show();
                 startActivity(new Intent(RegisterActivity.this, StartActivity.class));
                 finish();
